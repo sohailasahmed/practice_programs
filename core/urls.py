7fcc167dfc527e167560042fb084ce35f182215f
sohailasahmed <sohailasahmed@gmail.com>
2026-05-24 03:05:22 -0700

    model concept and Dynamic URl

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('calculaor/',calview.calculator,name='calculaor'),
     path('result/',calview.res),
     path('news/',newsview.mynews),
+    path('news/<n_id>',newsview.NewsDetails)
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from news.models import News
 
 # Create your views here.
 def mynews(request):
-    newsall='Demo News, Welcome to news page'
+    newsall=News.objects.all().order_by('news_title')
     data={
         'newsall':newsall
+    }
+    return render(request,'news.html',data)
+
+def NewsDetails(request,n_id):
+    Newsdetails=News.objects.get(id=n_id)
+    data={
+        'Newsdetails':Newsdetails
     }
     return render(request,'news.html',data)
