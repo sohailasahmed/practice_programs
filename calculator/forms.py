3313cc1eef6e191e4fa3188c661bd332cf1be471
sohailasahmed <sohailasahmed@gmail.com>
2026-06-01 09:06:23 -0700

    Cleaning Repo structue

--- calculator/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class calform(forms.Form):
-    Name=forms.CharField(label="Subject",widget=forms.TextInput({'class':'form-control'}))
-    Description=forms.CharField(label="Message",widget=forms.Textarea({'class':'form-control'}))
--- news/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from tinymce.models import HTMLField
-from autoslug import AutoSlugField
-
-# Create your models here.
-class News(models.Model):
-    news_title=models.CharField(max_length=10)
-    news_desc= HTMLField()
-    news_slug=AutoSlugField(populate_from='news_title',unique=True,null=True,default=None)
