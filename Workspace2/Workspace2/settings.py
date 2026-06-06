b39b07e4b1f650ed46e06bbdeed5506424acd94b
sohailasahmed <sohailasahmed@gmail.com>
2026-06-06 07:55:56 -0700

    Secret Keys Hidden

--- Workspace2/Workspace2/settings.py
+++ Workspace2/Workspace2/settings.py
@@ -18,12 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qf3*h++q@!$$_*u%z#ai_ty##4tsy2@qah-=8&ki&h+s1r_4%z'
+SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
